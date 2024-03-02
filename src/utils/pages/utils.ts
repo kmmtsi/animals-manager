88a8a87caf3182fb827dc3b8121a2175f04ec758
sei <kmmtsi@gmail.com>
2024-03-02 11:46:32 +0900

    no comment

---
+++ src/utils/pages/utils.ts
@@ -0,0 +1,24 @@
+import { faFileImport, faListUl, faPlus, faUser } from "@fortawesome/free-solid-svg-icons";
+
+export const navItems = [
+  {
+    path: "",
+    name: "動物一覧",
+    icon: faListUl,
+  },
+  {
+    path: "create",
+    name: "新規追加",
+    icon: faPlus,
+  },
+  // {
+  //   path: "import",
+  //   name: "インポート",
+  //   icon: faFileImport,
+  // },
+  {
+    path: "account",
+    name: "アカウント",
+    icon: faUser,
+  },
+];
